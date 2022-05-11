576ffa2b1b38ca3ea7381067d1ed19c061966236
LubnaMasood <lubnamasood40@gmail.com>
2022-05-11 10:10:07 +0000

    update to test script and testing.txt added

--- test.sh
+++ test.sh
@@ -7,8 +7,8 @@ do
   sudo apt-get install python3 python3-pip python3-venv
   python3 -m venv venv
   source venv/bin/activate
-  pip3 install -r requirements.txt
-  python3 -m pytest --cov=application --cov-report=xml --junitxml=junit/test-results.xml
+  pip3 install -r testing.txt
+  python3 -m pytest --cov
   deactivate
   cd ..
 done
